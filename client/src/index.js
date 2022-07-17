9788e00f0160b3af48342565d09e84ec95410973
nxNghia <nghia1421999@gmail.com>
2022-07-17 22:53:13 +0700

    downgrade react version

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from "redux"
 import thunk from 'redux-thunk'
 import './index.css';
@@ -17,14 +17,13 @@ const store = createStore(
     )
 )
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>    
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
